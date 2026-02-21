7ff10eb49e1219ca703b3dee847dd828410378fc
caculadoranutricional <calculadoranutricional@gmail.com>
2026-02-21 00:00:57 -0300

    fix: correct static adapter import path

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,9 +1,19 @@
-import { qwikStatic } from "@builder.io/qwik-city/staticadapter";
+import { qwikStatic } from "@builder.io/qwik-city/adapters/static/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
+
 export default extendConfig(baseConfig, () => {
   return {
-    build: { ssr: true, rollupOptions: { input: ["@qwik-city-plan"] } },
-    plugins: [qwikStatic({ origin: "https://calculadoranutricional.github.io" })],
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ["@qwik-city-plan"],
+      },
+    },
+    plugins: [
+      qwikStatic({
+        origin: "https://calculadoranutricional.github.io",
+      }),
+    ],
   };
 });
